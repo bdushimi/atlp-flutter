1293665f05a3106b753434ec38fe77165714fb7e
bdushimi <lespharaons@hotmail.com>
2022-05-05 23:44:37 +0200

    Adds Interactivity & States on Widgets

--- flutter-firebase/greeting-text/greeting_text/lib/main.dart
+++ flutter-firebase/greeting-text/greeting_text/lib/main.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(HelloFlutterApp());
 
-class HelloFlutterApp extends StatelessWidget {
+class HelloFlutterApp extends StatefulWidget {
+  @override
+  _HelloFlutterAppState createState() => _HelloFlutterAppState();
+}
+
+String englishGreeting = "Hello Flutter !";
+String kinyarwandaGreeting = "Muraho Flutter !";
+
+class _HelloFlutterAppState extends State<HelloFlutterApp> {
+  String displayText = englishGreeting;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child : Text ("Hello Flutter")
-        )
-      ));
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Hi dushi!"),
+              leading: Icon(Icons.home),
+              actions: <Widget>[
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        displayText = displayText == englishGreeting
+                            ? kinyarwandaGreeting
+                            : englishGreeting;
+                      });
+                    },
+                    icon: Icon(Icons.refresh))
+              ],
+            ),
+            body: Center(
+                child: Text(
+                  displayText, 
+                  style: TextStyle(fontSize: 24)))));
   }
 }
