fcfbeac8bb27e7b0b448d8a0f16bc0cd4cfe2e6a
bdushimi <lespharaons@hotmail.com>
2022-05-07 16:34:10 +0200

    Adds themeData using new API

--- flutter-firebase/hello_layouts/lib/main.dart
+++ flutter-firebase/hello_layouts/lib/main.dart
@@ -9,6 +9,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: const ColorScheme(
+          primary: Color(0xff3700B3),
+          secondary: Color(0xff03DAC6),
+          background: Color(0xffFFFFFF),
+          brightness: Brightness.light,
+          error: Color(0xffB00020),
+          onBackground: Color(0xff000000),
+          onError: Color(0xffFFFFFF),
+          onPrimary: Color(0xffFFFFFF),
+          onSurface: Color(0xffFFFFFF),
+          onSecondary: Color(0xff000000),
+          surface: Color(0xff000000),
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Build Layouts with Flutter"),
@@ -26,18 +42,9 @@ class MyApp extends StatelessWidget {
           child: const Icon(Icons.lightbulb_outlined),
         ),
         persistentFooterButtons: <Widget>[
-          IconButton(
-            onPressed: () {}, 
-          icon: const Icon(Icons.add_comment)
-          ),
-          IconButton(
-            onPressed: () {}, 
-          icon: const Icon(Icons.add_alarm)
-          ),
-          IconButton(
-            onPressed: () {}, 
-          icon: const Icon(Icons.add_location)
-          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.add_comment)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.add_alarm)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.add_location)),
         ],
       ),
     );
