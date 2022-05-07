d28b04e0213bbc6033ceac46e04b874fd3f2b6b0
bdushimi <lespharaons@hotmail.com>
2022-05-08 00:19:26 +0200

    Cleanup & setup project

---
+++ flutter-firebase/doctor_consultant_app_ui/lib/screens/auth/components/sign_in_form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+import '../../../constants.dart';
+import 'sign_up_form.dart';
+
+class SignInForm extends StatelessWidget {
+  SignInForm({
+    Key? key,
+    required this.formKey,
+  }) : super(key: key);
+
+  final GlobalKey formKey;
+
+  late String _email, _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const TextFieldName(text: "Email"),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: "test@email.com"),
+            validator: EmailValidator(errorText: "Use a valid email!"),
+            onSaved: (email) => _email = email!,
+          ),
+          const SizedBox(height: defaultPadding),
+          const TextFieldName(text: "Password"),
+          TextFormField(
+            // We want to hide our password
+            obscureText: true,
+            decoration: const InputDecoration(hintText: "******"),
+            validator: (text) {
+              if (text == null || text.isEmpty) {
+                return 'Can\'t be empty';
+              }
+              if (text.length < 4) {
+                return 'Too short';
+              }
+              return null;
+            },
+            onSaved: (password) => _password = password!,
+          ),
+          const SizedBox(height: defaultPadding),
+        ],
+      ),
+    );
+  }
+}
