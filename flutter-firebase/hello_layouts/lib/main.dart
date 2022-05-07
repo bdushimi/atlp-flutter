808131c7b3d19fecdeec2215cc3c4e801b8356b0
bdushimi <lespharaons@hotmail.com>
2022-05-07 03:17:59 +0200

    Uses Scaffold

--- flutter-firebase/hello_layouts/lib/main.dart
+++ flutter-firebase/hello_layouts/lib/main.dart
@@ -3,20 +3,42 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Build Layouts with Flutter"),
         ),
         body: const Center(
-          child: Text("Hello Flutter Layouts",
-          style: TextStyle(fontSize: 24),
+          child: Text(
+            "Hello Flutter Layouts",
+            style: TextStyle(fontSize: 24),
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("You rang!");
+          },
+          child: const Icon(Icons.lightbulb_outlined),
+        ),
+        persistentFooterButtons: <Widget>[
+          IconButton(
+            onPressed: () {}, 
+          icon: const Icon(Icons.add_comment)
+          ),
+          IconButton(
+            onPressed: () {}, 
+          icon: const Icon(Icons.add_alarm)
+          ),
+          IconButton(
+            onPressed: () {}, 
+          icon: const Icon(Icons.add_location)
+          ),
+        ],
       ),
     );
   }
