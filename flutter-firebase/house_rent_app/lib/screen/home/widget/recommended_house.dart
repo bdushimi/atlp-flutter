3a5f410bea5bb2d694b73e1dac5d1c5b8c15493c
bdushimi <lespharaons@hotmail.com>
2022-05-13 01:02:54 +0200

    adds icons & caption to house gallery

--- flutter-firebase/house_rent_app/lib/screen/home/widget/recommended_house.dart
+++ flutter-firebase/house_rent_app/lib/screen/home/widget/recommended_house.dart
@@ -12,7 +12,7 @@ class RecommendedHouse extends StatelessWidget {
     return Container(
         height: 340,
         child: ListView.separated(
-          scrollDirection: Axis.horizontal,
+            scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) => Container(
                   height: 300,
                   width: 200,
@@ -21,21 +21,59 @@ class RecommendedHouse extends StatelessWidget {
                     children: [
                       Container(
                         decoration: BoxDecoration(
-                           image: DecorationImage(
-                             image: AssetImage(
-                               recommendedHouses[index].imageUrl,
-                             ),
-                             fit: BoxFit.cover
-                             ),
+                          image: DecorationImage(
+                              image: AssetImage(
+                                recommendedHouses[index].imageUrl,
+                              ),
+                              fit: BoxFit.cover),
                         ),
                       ),
                       Positioned(
-                        right: 15,
-                        top: 15,
-                        child: CircularIconWidget(
-                          iconUrl: 'assets/icons/mark.svg',
-                          color: Theme.of(context).colorScheme.secondary,
-                        )),
+                          right: 15,
+                          top: 15,
+                          child: CircularIconWidget(
+                            iconUrl: 'assets/icons/mark.svg',
+                            color: Theme.of(context).colorScheme.secondary,
+                          )),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          color: Colors.white54,
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    recommendedHouses[index].name,
+                                    style: Theme.
+                                    of(context)
+                                    .textTheme
+                                    .headline1!
+                                    .copyWith(fontSize: 16, fontWeight: FontWeight.bold)
+                                    ,),
+                                    const SizedBox(height: 5,),
+                                  Text(recommendedHouses[index].address,
+                                  style: Theme.
+                                    of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .copyWith(fontSize: 14, fontWeight: FontWeight.bold)
+                                    ,),
+                                ],
+                              ),
+                              CircularIconWidget(
+                                  iconUrl: 'assets/icons/heart.svg',
+                                  color:
+                                      Theme.of(context).colorScheme.secondary),
+                            ],
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                   decoration: BoxDecoration(
